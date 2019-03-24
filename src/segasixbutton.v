8d6a93a840a34dcd3bd710b7a09a4533d6c8db33
Bruno Freitas <bruno@brunofreitas.com>
2019-03-24 20:53:48 -0300

    Cosmetic changes

--- src/segasixbutton.v
+++ src/segasixbutton.v
@@ -18,12 +18,12 @@ module md_sixbutton_encoder (
     input z,     // Z button
     input md,    // Mode button
     input clk,   // External clock for the timeout reset (i.e. 20Mhz)
-    output reg p1 = 1, // DB9 Pin 1 to Sega Mega Drive
-    output reg p2 = 1, // DB9 Pin 2 to Sega Mega Drive
-    output reg p3 = 1, // DB9 Pin 3 to Sega Mega Drive
-    output reg p4 = 1, // DB9 Pin 4 to Sega Mega Drive
-    output reg p6 = 1, // DB9 Pin 6 to Sega Mega Drive
-    output reg p9 = 1  // DB9 Pin 9 to Sega Mega Drive
+    output reg p1 = 1'b1, // DB9 Pin 1 to Sega Mega Drive
+    output reg p2 = 1'b1, // DB9 Pin 2 to Sega Mega Drive
+    output reg p3 = 1'b1, // DB9 Pin 3 to Sega Mega Drive
+    output reg p4 = 1'b1, // DB9 Pin 4 to Sega Mega Drive
+    output reg p6 = 1'b1, // DB9 Pin 6 to Sega Mega Drive
+    output reg p9 = 1'b1  // DB9 Pin 9 to Sega Mega Drive
 );
 
 parameter CLK_FREQ = 20000000; // For a 20Mhz oscillator
@@ -46,7 +46,6 @@ always @(p7 or posedge clk) begin
 
         // Reset transition at every 2ms (500Hz)
         if (clk_counter == 0) begin
-            $display("Timeout!\n");
             transition <= 0;
             clk_counter <= CLK_FREQ / 500;
         end
@@ -57,47 +56,22 @@ always @(*) begin
     if (p7) begin
         case (transition)
             0, 1, 2, 3, 6, 7: begin
-                p1 <= up;
-                p2 <= dw;
-                p3 <= lf;
-                p4 <= rg;
-                p6 <= b;
-                p9 <= c;
+                {p1, p2, p3, p4, p6, p9} <= {up, dw, lf, rg, b, c};
             end
             4, 5: begin
-                p1 <= z;
-                p2 <= y;
-                p3 <= x;
-                p4 <= md;
-                p6 <= 1;
-                p9 <= 1;
+                {p1, p2, p3, p4, p6, p9} <= {z, y, x, md, 1'b1, 1'b1};
             end
         endcase
     end else begin
         case (transition)
             0, 1, 2, 7: begin
-                p1 <= up;
-                p2 <= dw;
-                p3 <= 0;
-                p4 <= 0;
-                p6 <= a;
-                p9 <= st;
+                {p1, p2, p3, p4, p6, p9} <= {up, dw, 1'b0, 1'b0, a, st};
             end
             3, 4: begin
-                p1 <= 0;
-                p2 <= 0;
-                p3 <= 0;
-                p4 <= 0;
-                p6 <= a;
-                p9 <= st;
+                {p1, p2, p3, p4, p6, p9} <= {1'b0, 1'b0, 1'b0, 1'b0, a, st};
             end
             5, 6: begin
-                p1 <= 1;
-                p2 <= 1;
-                p3 <= 1;
-                p4 <= 1;
-                p6 <= a;
-                p9 <= st;
+                {p1, p2, p3, p4, p6, p9} <= {1'b1, 1'b1, 1'b1, 1'b1, a, st};
             end
         endcase
     end
